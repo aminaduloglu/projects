d3819031186d5a13cc0ea43db1d1e45d4857ae83
aminaduloglu <118767453+aminaduloglu@users.noreply.github.com>
2023-05-24 23:41:13 +0300

    Add files via upload

---
+++ PR5V8/pr5(1)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//2. Дан текст, среди элементов которого есть символы «двоеточие». Получить все символы, 
+//расположенные между первым и вторым встреченными символами «двоеточие». Если 
+//второго двоеточия нет, то получить все символы, расположенные после единственного 
+//двоеточия. В качестве текста для тестового примера (тестовых примеров) взять не менее 
+//пяти предложений.
+//PR5V8_Duloglu
+
+namespace ColonSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            int firstColonIndex = text.IndexOf(':');
+            int secondColonIndex = text.IndexOf(':', firstColonIndex + 1);
+
+            if (secondColonIndex == -1)
+            {
+                string result = text.Substring(firstColonIndex + 1);
+                Console.WriteLine("Символы после первого двоеточия: " + result);
+            }
+            else
+            {
+                string result = text.Substring(firstColonIndex + 1, secondColonIndex - firstColonIndex - 1);
+                Console.WriteLine("Символы между первым и вторым двоеточиями: " + result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
