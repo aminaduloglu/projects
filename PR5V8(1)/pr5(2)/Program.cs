ab29fba21b0ccbab41aa5d867836a881fb9a26f8
aminaduloglu <118767453+aminaduloglu@users.noreply.github.com>
2023-05-24 23:43:25 +0300

    Add files via upload

---
+++ PR5V8(1)/pr5(2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+//1. Определить, является введённое слово «перевёртышем» (например, «потоп», «казак»).
+//PR5V8_Duloglu
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine();
+
+            bool isPalindrome = true;
+
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("Слово является перевертышем.");
+            }
+            else
+            {
+                Console.WriteLine("Слово не является перевертышем.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
